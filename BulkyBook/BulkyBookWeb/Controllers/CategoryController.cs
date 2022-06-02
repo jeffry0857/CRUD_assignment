85dca6f81a694f5a5cfcfac278a73b37553809e7
Jeffry <jeffry0857@gmail.com>
2022-06-02 08:04:25 +0800

    updated

--- BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -27,6 +27,11 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            // 如果輸入的類別名稱跟Display Order一樣則會跳出警示
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "類別名稱不能跟Display Order一樣");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
